054d2a5b6a98d27ffe66bf896e882abf513dccfa
valen725 <juana.martinez@utp.edu.co>
2024-09-22 16:26:58 -0500

    cambios groseros

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-class main(){
-    print("Juana y Sebas se fueron para Ukumary")
-}
+from ui.ui_module import UI
+from reduccion.reduccion_module import Reduccion
+
+class Main:
+    def __init__(self):
+        self.minterms = []
+        self.ui = UI()
+
+    def ejecutar(self):
+        # Mostrar el menú
+        self.ui.mostrar_menu()
+        
+        # Pedir los minitérminos al usuario
+        self.minterms = self.ui.ingresar_minterminos()
+
+        # Crear una instancia de la clase Reduccion con los minitérminos
+        reduccion = Reduccion(self.minterms)
+
+        # Mostrar la tabla con los minitérminos y sus representaciones binarias
+        reduccion.mostrar_tabla()
+
+if __name__ == "__main__":
+    main = Main()
+    main.ejecutar()
+
